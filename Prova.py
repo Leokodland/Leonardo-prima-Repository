b3cc83b47626c4ec6964293a186022a8aa3bae81
Leokodland <marty_79@live.it>
2024-07-20 14:07:14 +0200

    Update Prova.py

--- Prova.py
+++ Prova.py
@@ -1,1 +1,8 @@
+import random
 
+elementi = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+lunghezza = int(input("inserisci la lunghezza della password:"))
+password= ""
+for i in range(lunghezza):
+    password +=random.choice(elementi)
+print(password)
